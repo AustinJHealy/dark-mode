a8c17f9a782cd2b7522d5fbd452b109ce5413aea
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-06-11 18:57:50 -0400

    formatted code, final version

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = (initialValue) => {
-  const [darkMode, setDarkMode] = useLocalStorage('darkMode', initialValue);
-  const body = document.querySelector('body');
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", initialValue);
+  const body = document.querySelector("body");
   useEffect(() => {
-     if (darkMode){
-            body.classList.add('dark-mode')
-        } else {
-            body.classList.remove('dark-mode')
+    if (darkMode) {
+      body.classList.add("dark-mode");
+    } else {
+      body.classList.remove("dark-mode");
     }
   }, [darkMode, body.classList]);
   return [darkMode, setDarkMode];
